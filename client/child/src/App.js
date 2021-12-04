65e6867997d5b2760443abd882a4aa54c52dc698
MarkMiso <grassimarco.cs@gmail.com>
2021-12-04 13:05:57 +0100

    feat: child client setup

---
+++ client/child/src/App.js
@@ -0,0 +1,17 @@
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <h1 class="mt-36 text-center text-8xl">
+          pepeNotes
+        </h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
