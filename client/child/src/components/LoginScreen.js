52ae3dba992fc5d9476fa47bc2b965c2d4efbb49
MarkMiso <grassimarco.cs@gmail.com>
2021-12-10 02:38:22 +0100

    feat: login ui

--- client/child/src/components/LoginScreen.js
+++ client/child/src/components/LoginScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { useAuth } from "./AuthProvider"
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useAuth } from "./AuthProvider"
 
 export function LoginScreen() {
+  let { t } = useTranslation()
   let auth = useAuth();
   let navigate = useNavigate();
 
@@ -20,12 +22,38 @@ export function LoginScreen() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        username: <input name="username" type="text" />
-      </label>{" "}
-      <button type="submit">Login</button>
-    </form>
+    <div className="max-w-xl mx-auto px-2 flex flex-col sm:px-6 lg:px-8">
+      <div className="text-center mt-12">
+        <p className="text-5xl text-transparent bg-clip-text font-black bg-gradient-to-r from-yellow-500 to-red-500 pt-3">
+          Families Share 
+        </p>
+        <p className="font-semibold text-lg text-gray-500 pt-8">
+            {t('signin prompt')}
+        </p>
+      </div>
+
+      <form onSubmit={handleSubmit} className="mt-12 w-full p-5 shadow-lg rounded-md">
+        <div>
+          <label className="block font-semibold text-gray-500">
+            {t('username')}
+          </label>
+          <input name="username" type="text" className="transition ease-in-out duration-300 mt-1 appearance-none rounded-md w-full px-3 py-2 border-2 border-gray-300 font-semibold text-gray-900 focus:outline-none focus:ring-1 focus:border-1 focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"/>
+        </div>
+
+        <div className="mt-5">
+          <label className="block font-semibold text-gray-500">
+            {t('password')}
+          </label>
+          <input name="password" type="text" className="transition ease-in-out duration-300 mt-1 appearance-none rounded-md w-full px-3 py-2 border-2 border-gray-300 font-semibold text-gray-900 focus:outline-none focus:ring-1 focus:border-1 focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"/>
+        </div>
+        
+        <div className="mt-5">
+          <button type="submit" className="transition ease-in-out duration-300 w-full py-2 px-3 font-semibold rounded-md text-white bg-gradient-to-r from-yellow-500 to-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
+            {t('login')}
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- client/child/src/i18n.js
+++ client/child/src/i18n.js
@@ -7,26 +7,34 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   en: {
     translation: {
+      "username": "Username",
+      "password": "Password",
       "login": "Log In",
       "profile": "Profile",
       "settings": "Settings",
       "logout": "Log Out",
       "home": "Home",
+      "remember me": "Remember me",
       "dashboard": "Dashboard",
       "calendar": "Calendar",
-      "app description": "A "
+      "app description": "The Families_Share project is developing a social networking and awareness-raising platform dedicated to encouraging childcare and work/life balance",
+      "signin prompt": "Sign in to your account"
     }
   },
   it: {
     translation: {
+      "username": "Nome Utente",
+      "password": "Password",
       "login": "Accedi",
       "profile": "Profilo",
       "settings": "Impostazioni",
       "logout": "Esci",
       "home": "Casa",
+      "remember me": "Ricordami",
       "dashboard": "Pannello",
       "calendar": "Calendario",
-      "app description": "Il progetto Families_Share sta sviluppando una piattaforma volta a favorire la diffusione di reti sociali e la sensibilizzazione nell'ambito della cura all'infanzia e dell'equilibrio vita-lavoro"
+      "app description": "Il progetto Families_Share sta sviluppando una piattaforma volta a favorire la diffusione di reti sociali e la sensibilizzazione nell'ambito della cura all'infanzia e dell'equilibrio vita-lavoro",
+      "signin prompt": "Sign in to your account"
     }
   }
 };
